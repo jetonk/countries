f2fb87f225c2cf29793f4f788785f27ad06d6501
Jeton Kadrija <jeton.kadrija@gmail.com>
2020-08-23 00:20:27 +0200

    Link ListemItem onClick to make API Call for countries

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Row, Col, List } from 'antd';
 import { GlobalOutlined, RightOutlined } from '@ant-design/icons';
 import useCountries from 'api/countries';
@@ -9,15 +9,6 @@ const { Content } = Layout;
 
 const Main = () => {
   const [getCountriesByRegion, countries, errorMessage] = useCountries();
-
-  useEffect(() => {
-    getCountriesByRegion('europe');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const selectRegion = (region) => {
-    console.log('region', region)
-  }
 
   return (
     <Layout>
@@ -31,7 +22,10 @@ const Main = () => {
               bordered
               dataSource={Regions}
               renderItem={region => (
-                <List.Item style={styles.listItem} onClick={() => selectRegion(region)}>
+                <List.Item
+                  style={styles.listItem}
+                  onClick={() => getCountriesByRegion(region.name)}
+                >
                   {region.name}
                   <RightOutlined style={styles.listItemIcon} />
                 </List.Item>
